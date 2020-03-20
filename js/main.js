857863a3f86ff873ef708aecd8262760dc9458fe
jasper <buceocv@gmail.com>
2020-03-20 19:45:13 +0100

    Añadida grafica estadistica

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ const tableTimelineConfirmed = document.getElementById('timelineConfirmed');
 const tableTimelineDeath = document.getElementById('timelineDeath');
 const tableTimelineRecovered = document.getElementById('timelineRecovered');
 const timelineTable = document.getElementById('timelineTable');
+const canvasTimeline = document.getElementById('canvasTimeline');
 
 
 const getLocationById = async (idLocation) => {
@@ -42,8 +43,6 @@ const createNowTable = () =>{
 }
 
 const createTimelineTables = (data) => {
-
-     console.log(data);
     let tableValues =
         `   <tr>
                 <td colspan = '4' > Linea de tiempo en ESPAÑA </td>
@@ -75,10 +74,73 @@ const createTimelineTables = (data) => {
 
 }
 
+const generateDateTimelineArray =(data)=>{
+    let dataValues = []; 
+    Object.keys(data.location.timelines.confirmed.timeline).forEach((value)=>{
+        let dateValue = new Date(value); 
+        dataValues.push(dateValue.toLocaleDateString());
+    })
+    return dataValues;
+}
+
+const generateValuesTimelineArray = (data , type)=>{
+    let dataValues = []; 
+        if (type === 'confirmed'){
+            Object.keys(data.location.timelines.confirmed.timeline).map((key)=>{
+                dataValues.push(data.location.timelines.confirmed.timeline[key]);
+            })
+        }
+        if(type === 'death'){
+            Object.keys(data.location.timelines.deaths.timeline).map((key)=>{
+                dataValues.push(data.location.timelines.deaths.timeline[key]);
+            })
+        }
+        if(type === 'recovered'){
+            Object.keys(data.location.timelines.recovered.timeline).map((key)=>{
+                dataValues.push(data.location.timelines.recovered.timeline[key]);
+            })
+        }
+    return dataValues;
+}
+
+const renderingCanvas = (data) =>{
+
+    let confirmedNow = {
+        label : 'Confirmados',
+        data : generateValuesTimelineArray(data , 'confirmed') , 
+        lineTension: 0.3 ,
+        backgroundColor:'rgba(94, 25, 163, 0.4)'
+    }
+    let deathNow = {
+        label : 'Fallecidos',
+        data : generateValuesTimelineArray(data , 'death') , 
+        lineTension: 0.3 ,
+        backgroundColor:'rgba(252, 2, 2, 0.5)'
+    }
+    let recovered = {
+        label : 'Recuperados',
+        data : generateValuesTimelineArray(data , 'recovered') , 
+        lineTension: 0.3 ,
+        backgroundColor:'rgba(124, 249, 0, 0.4)'
+    }
+
+    let days = {
+        labels : generateDateTimelineArray(data) , 
+        datasets: [confirmedNow ,deathNow ,recovered ]
+    }
+
+    let nowChart = new Chart(canvasTimeline, {
+        type : 'line',
+        data : days
+    })
+
+}
+
 
 const formatData = (data) => {
     renderingNowData(data);
     renderingTimeLineData(data);
+    renderingCanvas(data);
 }
 
 const init = () => {
